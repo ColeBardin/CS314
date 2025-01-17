f72968ae922eeae1dc9237e86b13c2fe9ed193a4
Cole Bardin <cab572@drexel.edu>
2025-01-16 22:41:39 -0500

    added hw1

---
+++ hw1/hw1.c
@@ -0,0 +1,63 @@
+#include <u.h>
+#include <libc.h>
+
+#define BUTTON 20 /* Pin 20 - Button Input */
+#define LED 21 /* Pin 21 - LED Output */
+
+typedef enum {
+	LED_OFF,
+	LED_ON,
+	LED_FLASH
+} led_state_e;
+
+int read_button(int fd);
+
+void main(){
+	int fd;
+	int button;
+	int led_flash = 0;
+	led_state_e state = LED_OFF;
+
+	bind("#G", "/dev", MAFTER);
+	fd = open("/dev/gpio", ORDWR);
+	fprint(fd, "function %d in", BUTTON);
+	fprint(fd, "pulldown %d", BUTTON);
+	fprint(fd, "function %d out", LED);
+	fprint(fd, "pulldown %d", LED);
+
+	for(;;){
+		button = read_button(fd);
+		if(button){
+			print("button pressed\n");
+			while(read_button(fd)) sleep(10);
+			print("button released\n");
+			state++;
+			if(state > LED_FLASH) state = LED_OFF;
+			print("new state: %d\n", state);
+		}
+
+		switch(state){
+			case LED_OFF:
+				fprint(fd, "set %d 0", LED);
+				break;
+			case LED_ON:
+				fprint(fd, "set %d 1", LED);	
+				break;
+			case LED_FLASH:
+				fprint(fd, "set %d %d", LED, led_flash);
+				led_flash = !led_flash;
+				break;
+		}
+		sleep(100);
+	}		
+}
+
+int read_button(int fd){
+	char buf[16] = {0};
+	uvlong gvals;
+
+	read(fd, buf, 16);
+	gvals = strtoull(buf, nil, 16);
+
+	return 1 & (gvals >> BUTTON);
+}
